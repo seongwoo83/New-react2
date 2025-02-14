fd7ad7c7912d68b52a6aa01806a3d483fe3bfc38
jsw <jeongseongwoo83@daum.net>
2025-02-14 22:27:22 +0900

    [refactor] 오타수정

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#community">Community</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
