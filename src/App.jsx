bd16c983ac2a4a826719306eb0a2a817c37a38f1
jsw <jeongseongwoo83@daum.net>
2025-02-16 23:01:17 +0900

    [feat] react-router-dom 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import data from './data';
 import {Container, Navbar, Nav, Row, Col} from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
   let [shoes] = useState(data)
 
+  // Route - 각 페이지를 의미
+
   return (
     <>
     <div className="App">
+
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Wooniverse</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#community">Community</Nav.Link>
+            <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
+
+      <Routes>
+        <Route path='/' element={
+          <>
+            <div className="main-bg"></div>
+            <Card shoes={shoes}/>
+          </>
+          } />
+        <Route path='/detail' element={<Detail shoes={shoes}></Detail>} />
+      </Routes>
+
       
-      <Container>
-        <Row>
-        {
-          shoes.map((a,i)=>{
-            return <Card shoes={shoes[i]} index={i} key={i}></Card> 
-          })
-        }
-        </Row>
-      </Container>
+      
 
     </div>
     </>
@@ -42,12 +46,40 @@ function App() {
 
 function Card(props){
   return(
-    <Col className='col-4'>
-      <img src={'https://codingapple1.github.io/shop/shoes'+(props.index + 1)+'.jpg'} width="80%"/>
-      <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.price}</p>
-    </Col>
+  <Container>
+    <Row>
+    {
+      props.shoes.map((v,i)=>{
+        return (
+        <Col className='col-4' key={i}>
+          <img src={'https://codingapple1.github.io/shop/shoes'+(i + 1)+'.jpg'} width="80%"/>
+          <h4>{v.title}</h4>
+          <p>{v.price}</p>
+        </Col>
+        ) 
+      })
+    }
+    </Row>
+  </Container>
   )
 }
 
+
+function Detail(props){
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={"https://codingapple1.github.io/shop/shoes"+(props.i)+".jpg"} width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{props.title}</h4>
+          <p>{props.content}</p>
+          <p>{props.price원}</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+    </div> 
+  )
+}
 export default App
