b0d8b33bdc2501840645a631997596800ff0b064
jsw <jeongseongwoo83@daum.net>
2025-03-09 22:25:57 +0900

    [feat] useNavigate사용

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,20 @@
 import { Row, Col, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 function Card(props){
+
+  const navigate = useNavigate();
+
+  const toDetail = (index)=>{
+    navigate("/detail/"+index);
+  }
+
   return(
   <Container>
     <Row>
     {
       props.shoes.map((v,i)=>{
         return (
-        <Col className='col-4' key={i}>
+        <Col className='col-4' key={i} onClick={()=>toDetail(i)}>
           <img src={'https://codingapple1.github.io/shop/shoes'+(i + 1)+'.jpg'} width="80%"/>
           <h4>{v.title}</h4>
           <p>{v.price}</p>
