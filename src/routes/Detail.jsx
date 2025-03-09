f011d06e617e1d7c9dcd2548d6d54d7ca918d844
jsw <jeongseongwoo83@daum.net>
2025-03-09 21:43:24 +0900

    [feat] useEffect사용

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,24 +1,40 @@
 /* eslint-disable react/prop-types */
 
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 
-let YellowBtn = styled.button`
-  background: ${props => props.bg};
-  color: ${props => props.bg == 'blue' ? 'white' : 'black'};
-  padding: 10px;
-`; // props 사용가능
-
-let NewBtn = styled.button(YellowBtn)`
-  padding: 20px;
-`
-
 function Detail(props){
+
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+  let [input, setInput] = useState('');
 
   let {id} = useParams();
   let setProdct = props.shoes.find((v)=> v.id == id)
 
+  useEffect(() => {  // 렌더링이 다 진행되고 나서 실행됨
+    let timer = setTimeout(() => { setAlert(false); }, 2000);
+
+    return ()=>{ // useEffect동작 전에 실행되는 코드(aka. clean up function), mount시에는 실행되지 않음, unmount시에는 실행됨
+      clearTimeout(timer);
+    }
+  }, [count]); //dependency -> 해당 변수가 변할때마다 실행, 없으면 mount시에만 실행됨
+
+  useEffect(()=>{
+    if(isNaN(input) == true){
+      setInput(input.slice(0,-1));
+      window.alert('숫자만 입력가능합니다.');
+    }
+  }, [input])
+
+  useEffect(()=>{}) // 1. 재렌더링마다 코드실행
+  useEffect(()=>{}, []) // 2. mount시 1회 코드실행
+  //useEffect(()=>{}, [aaa]) // 3. aaa라는 state가 update되면 실행됨
+  useEffect(()=>{
+    return (()=>{})
+  })  // 4. useEffect실행 전에 실행됨
 
   return (
     <div className="container">
@@ -26,12 +42,19 @@ function Detail(props){
       {
         id < props.shoes.length? 
         <>
-          <YellowBtn bg='blue'>버튼</YellowBtn>
-          <YellowBtn bg='orange'>버튼</YellowBtn>
+          {
+            alert == true
+          ? <div className="alert alert-warning">
+              2초이내 구매시 할인
+            </div>
+          : null
+          }
+          <button onClick={()=>setCount(count + 1)}>{count}버튼</button>
           <div className="col-md-6">
             <img src={`https://codingapple1.github.io/shop/shoes${ Number(setProdct.id)+1 }.jpg`} width="100%" />
           </div>
           <div className="col-md-6">
+            <input type="text" onChange={(e)=>{setInput(e.target.value)}} value={input}/>
             <h4 className="pt-5">{setProdct.title}</h4>
             <p>{setProdct.content}</p>
             <p>{setProdct.price}원</p>
